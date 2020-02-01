08a6c7b1724cc37a08589746460dafcccd4f5282
Rohit Kumar <rkumar@dimagi.com>
2020-02-01 20:13:05 +0530

    Adding test case

---
+++ test/integration/default/serverspec/test_spec.rb
@@ -0,0 +1,17 @@
+require 'serverspec'
+set :backend, :exec
+
+cur_dir = Dir.pwd
+test_command = cur_dir + '/test/test.sh'
+
+describe package('logstash') do
+    it { should be_installed }
+  end
+
+describe package('filebeat') do
+    it { should be_installed }
+  end
+
+describe command(test_command) do
+        its(:exit_status) {should eq 0}
+end
---
+++ test/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GOROOT=~/go
+PATH=$GOROOT/bin:$PATH
+
+wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
+tar -xvf go1.12.4.linux-amd64.tar.gz
+
+wget https://github.com/magnusbaeck/logstash-filter-verifier/releases/download/1.6.0/logstash-filter-verifier_1.6.0_linux_386.tar.gz
+tar -xvf logstash-filter-verifier_1.6.0_linux_386.tar.gz
+
+TESTS_PATH=${PWD}/test/logstash/
+FILTERS_PATH=/etc/logstash/conf.d/10-filters.conf
+LOGSTASH_BIN=/usr/share/logstash/bin/logstash
+
+./logstash-filter-verifier ${TESTS_PATH} ${FILTERS_PATH} --logstash-path ${LOGSTASH_BIN}/logstash
+
