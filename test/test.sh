60f70035cd9fad804cbfdf7f3ab593d385904008
Simon Kelly <skelly@dimagi.com>
2020-02-03 17:34:12 +0200

    install test dependencies as part of provisioning

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-GOROOT=~/go
+set -ve
+
+GOROOT=/usr/local/go
 PATH=$GOROOT/bin:$PATH
 
-wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
-tar -xf go1.12.4.linux-amd64.tar.gz
-
-wget https://github.com/magnusbaeck/logstash-filter-verifier/releases/download/1.6.0/logstash-filter-verifier_1.6.0_linux_386.tar.gz
-tar -xf logstash-filter-verifier_1.6.0_linux_386.tar.gz
+LOGSTASH_VERIFIER=/usr/local/logstash-filter-verifier/logstash-filter-verifier
 
 TESTS_PATH=${PWD}/test/logstash/
 FILTERS_PATH=/etc/logstash/conf.d/10-filters.conf
 LOGSTASH_BIN=/usr/share/logstash/bin/logstash
 
-./logstash-filter-verifier ${TESTS_PATH} ${FILTERS_PATH} --logstash-path ${LOGSTASH_BIN}/logstash --sockets
+$LOGSTASH_VERIFIER ${TESTS_PATH} ${FILTERS_PATH} --logstash-path ${LOGSTASH_BIN}/logstash --sockets
 
