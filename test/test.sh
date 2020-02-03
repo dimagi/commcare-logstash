fb761a0d87330d8011baeeb794b1612997861b38
Simon Kelly <skelly@dimagi.com>
2020-02-03 13:40:53 +0200

    remove verbose tar output

--- test/test.sh
+++ test/test.sh
@@ -4,10 +4,10 @@ GOROOT=~/go
 PATH=$GOROOT/bin:$PATH
 
 wget https://dl.google.com/go/go1.12.4.linux-amd64.tar.gz
-tar -xvf go1.12.4.linux-amd64.tar.gz
+tar -xf go1.12.4.linux-amd64.tar.gz
 
 wget https://github.com/magnusbaeck/logstash-filter-verifier/releases/download/1.6.0/logstash-filter-verifier_1.6.0_linux_386.tar.gz
-tar -xvf logstash-filter-verifier_1.6.0_linux_386.tar.gz
+tar -xf logstash-filter-verifier_1.6.0_linux_386.tar.gz
 
 TESTS_PATH=${PWD}/test/logstash/
 FILTERS_PATH=/etc/logstash/conf.d/10-filters.conf
